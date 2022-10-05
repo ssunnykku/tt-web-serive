65e71316d6994682070d12d9293c926a6966b40d
hyeonjeong4031 <hjn40300@gmail.com>
2022-10-05 14:42:35 +0900

    update app.js

---
+++ back/src/app.js
@@ -0,0 +1,32 @@
+import cors from "cors";
+import express from "express";
+import { errorMiddleware } from "./middlewares/errorMiddleware";
+const app = express();
+app.use(cors());
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+
+app.get("/",(req, res)=>{
+  console.log(req,body);
+  res.send("data project root api")
+});
+
+
+
+
+
+
+
+
+app.use(errorMiddleware);
+
+
+
+
+
+
+
+
+
+export {app}
