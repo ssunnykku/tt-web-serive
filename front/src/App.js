4289feca1d54085e1dc569d620cdb23de47afed0
HaEun <rjadmszhd777@naver.com>
2022-10-07 22:16:21 +0900

    signUpModal 기본뼈대구조 및 CSS완성

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useReducer, createContext } from "react";
-
+import SignUpModal from "./components/signUpModal/SignUpModal";
 function App() {
   return (
     <div>
-      
+      <SignUpModal></SignUpModal>
     </div>
-  )
+  );
 }
 
 export default App;
