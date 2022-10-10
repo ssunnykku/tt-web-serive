58d69e88ca1e817f9822089d9c767bfe7d9ebc31
HaEun <rjadmszhd777@naver.com>
2022-10-10 16:35:12 +0900

    styles폴더이름 변경, 근혁님이랑 머지성공기원,,

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect, useReducer, createContext } from "react";
-import Modal from "../src/components/Modals/Modal";
+// import Modal from "../src/components/Modals/Modal";
 function App() {
-  return (
-    <div>
-      <Modal></Modal>
-    </div>
-  );
+  return <div>{/* <Modal></Modal> */}</div>;
 }
 
 export default App;
