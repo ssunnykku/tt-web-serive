789cb01efc2e775780b1be3b297b5dd7daa9f6f6
HaEun <rjadmszhd777@naver.com>
2022-10-17 17:14:36 +0900

    10/17 회원가입 api 리얼 성공

--- front/src/components/MainPage/MainPage1.js
+++ front/src/components/MainPage/MainPage1.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import LoginModal from "../LoginModal/LoginModal";
+import SignUpModal from "../signUpModal/SignUpModal";
 import "../../styles/mainpage/mainpage1.css";
 import StyledButton from "../../styles/commonstyles/Button";
 import NavBar from "../NavBar";
@@ -9,23 +10,27 @@ import { useNavigate } from "react-router-dom";
 const MainPage1 = () => {
   //열기, 닫기를 부모로부터 받아옴
   const [loginModalOpen, setLoginModalOpen] = useState(false);
-
+  const [signUpModalOpen, setSignUpModalOpen] = useState(false);
   const navigate = useNavigate();
   //로그인 모달창 노출
   const showLoginModal = () => {
     setLoginModalOpen(true);
+    setSignUpModalOpen(false);
   };
+  // //회원가입 모달창 노출
+  //   const showSignUpModal = () => {
+  //     setSignUpModalOpen(true);
+  //   };
 
   // const isLogin= !!userState.user;
-  const isLogin=false;
-   // const logout=()=>{
+  const isLogin = false;
+  // const logout=()=>{
   //   sessionStorage.removeItem('accessToken');
   //   localStorage.removeItem('refreshToken');
   //   dispatchEvent({type:'LOGOUT'});
   //   alert('로그아웃 완료')
   //   navigate('/')
   // }
-
 
   console.log("setLoginModalOpen", loginModalOpen);
 
@@ -40,20 +45,27 @@ const MainPage1 = () => {
             <a>에서 쉽고 빠르게</a>
           </div>
           <div className="buttonContainer">
-
-            <StyledButton onClick={()=>{
-              navigate('/network')
-            }}>챌린지</StyledButton>
-            {
-              isLogin===false?
+            <StyledButton
+              onClick={() => {
+                navigate("/network");
+              }}
+            >
+              챌린지
+            </StyledButton>
+            {isLogin === false ? (
               <StyledButton onClick={showLoginModal}>로그인</StyledButton>
-              :
-              <StyledButton >로그아웃</StyledButton>
-            }
-            
+            ) : (
+              <StyledButton>로그아웃</StyledButton>
+            )}
 
             {loginModalOpen && (
               <LoginModal setLoginModalOpen={setLoginModalOpen} />
+            )}
+            {signUpModalOpen && (
+              <SignUpModal
+                signUpModalOpen={signUpModalOpen}
+                setSignUpModalOpen={setSignUpModalOpen}
+              />
             )}
           </div>
         </div>
