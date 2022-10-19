bbf8a47040c4ea3654ce1214be6fee59b81ee6fa
HaEun <rjadmszhd777@naver.com>
2022-10-19 15:59:22 +0900

    메인페이지 데이터정리 완료

--- front/src/components/MainPage/MainPage5.js
+++ front/src/components/MainPage/MainPage5.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import NavBar from '../NavBar'
-import '../../styles/mainpage/mainpage5.css'
-import SeaLevel from '../Charts/SeaLevelGraph'
-import Disasters from '../Charts/Disasters'
+import React from "react";
+import NavBar from "../NavBar";
+import "../../styles/mainpage/mainpage5.css";
+import SeaLevel from "../Charts/SeaLevelGraph";
+import Disasters from "../Charts/Disasters";
 const MainPage5 = () => {
   return (
-    <div className='mainpage5'>
-      <NavBar/>
-      <div className='graphContainer'>
-        <div className='graphDescription'>
-        <a>안녕</a>
+    <div className="mainpage5">
+      <NavBar />
+      <div className="graphContainer">
+        <div className="graphDescription">
+          {/* <span className="disasterText">
+            내가 파괴 시킨 지구 나도 파괴하는 일 지구온난화 재해 발생 빈도가
+            늘어나고 있다. 이에 따라 피해를 받는 수가 점점 늘어나고 있다.
+          </span> */}
         </div>
-        <Disasters/>
+        <Disasters />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage5
+export default MainPage5;
