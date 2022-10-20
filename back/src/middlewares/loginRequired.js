747708a5a45d65790a3ee9516161e8bf2c20cb6d
hyeonjeong4031 <hjn40300@gmail.com>
2022-10-20 19:55:04 +0900

    console 제거

--- back/src/middlewares/loginRequired.js
+++ back/src/middlewares/loginRequired.js
@@ -5,7 +5,6 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 function loginRequired(req, res, next) {
-  //프론트에서 엑세스 토큰만 보낼때
   const accessToken = req.headers["authorization"]?.split(" ")[1] ?? "null";
   if (accessToken) {
     try {
@@ -16,51 +15,32 @@ function loginRequired(req, res, next) {
 
       next();
     } catch (error) {
-      // res.status(400).send("access token이 유효하지 않습니다.");
-
       const refreshToken = req.body.refreshToken;
-      // const refreshToken = userToken["authorization"]?.split(" ")[1] ?? "null";
 
       if (refreshToken) {
         try {
           const secretKey = process.env.JWT_SECRET_KEY;
-          // 1.  리프레시 토큰 검증
           const jwtDecoded = jwt.verify(refreshToken, secretKey);
           const userId = jwtDecoded.userId;
 
           const token = async () => {
-            console.log("refresh db 접근할때 사용할 userId:", userId);
             const token = await prisma.refreshToken.findUnique({
               where: {
                 userId: userId,
               },
             });
             const refreshFromDb = token.refreshToken;
-            //두번째 - front에서 준 refresh token과 db의 refresh token 과 동일한지 비교
 
             if (refreshFromDb != refreshToken) {
-              console.log("디비와 토큰이 다른 에러:");
-              console.log(refreshFromDb);
-              console.log(refreshToken);
-              const errorMessage = "refresh token이 유효하지 않습니다.";
               res.status(400).return("디비와 토큰이 다른 에러:");
             }
-            console.log("⭐️db에서 가져온 refresh=", refreshFromDb);
-            console.log("🦄req.body-decoded refresh=", refreshFromDb);
-            // res
-            //   .status(200)
-            //   .send("refresh token검증완료. access token을 발급해주세요");
-            // // return token.refreshToken;
           };
           const refreshFromDb = token();
-          console.log(refreshFromDb);
 
-          // refresh token 유효하면 access token생성 후, currentUserId와 함께 req로 보냄(req 두번보내도 되는건가..?)
           const accessToken = jwt.sign({ userId }, secretKey, {
             expiresIn: "1h",
           });
           req.currentUserId = userId;
-          console.log("!!!!!!!!!:==", accessToken, "!!!!!!!!!!!!!", userId);
           res.status(201).send(accessToken);
         } catch (error) {
           res
