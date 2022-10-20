e458899fed86c6b1232d8ab6e08c6361e0acf794
geunhyuk park <rmsgur95@gmail.com>
2022-10-20 17:04:30 +0900

    백폴더삭제

--- back/src/middlewares/dayCountsBetweenTodayAnd.js
+++
@@ -1,9 +0,0 @@
-function dayCountsBetweenTodayAnd(day) {
-  const today = new Date();
-  const date = new Date(day);
-  const dayCount = Math.floor((today - date) / (1000 * 60 * 60 * 24));
-
-  return dayCount;
-}
-
-export { dayCountsBetweenTodayAnd };
