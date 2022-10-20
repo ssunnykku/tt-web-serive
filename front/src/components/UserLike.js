247ba770e8b3f294ec693170e6a78022559bbf48
hyeonjeong4031 <hjn40300@gmail.com>
2022-10-20 16:58:29 +0900

    liked code 수정

--- front/src/components/UserLike.js
+++ front/src/components/UserLike.js
@@ -4,80 +4,81 @@ import * as Api from "../../src/api";
 import likeimg from "../images/userLiked/like.png";
 import likedimg from "../images/userLiked/liked.png";
 import "../styles/network/userLike.css";
-function UserLike({ challengeId}) {
+function UserLike({ challengeId }) {
   const [likeImoticon, setLikeImoticon] = useState(false);
-  const [countLike, setCountLike] = useState('0');
+  const [countLike, setCountLike] = useState("0");
   // const myId = 1;
-  const [likeId,setLikeId] = useState(1);
-  const [likeStatus,setLikeStatus]=useState(false)
-  const [checkUserId,setCheckUserId]=useState('')
-  useEffect(()=>{
-  
-  },[])
+  const [likeId, setLikeId] = useState(1);
+  const [likeStatus, setLikeStatus] = useState(false);
+  const [checkUserId, setCheckUserId] = useState("");
+  useEffect(() => {}, []);
   // useEffect(()=>{
   //   Api.post('liked').then((res)=>{
   //     likedId: likedId,
   //   })
   // })
-// useEffect(()=>{
-//   liked.forEach(x=>{
-//     x.userId === myId && setLike(true)
-//   })
-// },[])
+  // useEffect(()=>{
+  //   liked.forEach(x=>{
+  //     x.userId === myId && setLike(true)
+  //   })
+  // },[])
 
-//   useEffect(() => {
-//     Api.get("users", likedId).then((res) =>
-//       setCountLike(res.data.likeCount)
-//     );
-//   }, [likedId]);
+  //   useEffect(() => {
+  //     Api.get("users", likedId).then((res) =>
+  //       setCountLike(res.data.likeCount)
+  //     );
+  //   }, [likedId]);
 
+  //   useEffect(() => {
+  //     Api.get("like", likedId).then((res) =>
+  //       setLike(res.data.userStatus)
+  //     );
+  //   }, [likedId]);
 
-//   useEffect(() => {
-//     Api.get("like", likedId).then((res) =>
-//       setLike(res.data.userStatus)
-//     );
-//   }, [likedId]);
+  //   // const handleClick=useCallback(
+  //   //   async (e) => {
+  //   //     e.preventDefault();
 
-//   // const handleClick=useCallback(
-//   //   async (e) => {
-//   //     e.preventDefault();
+  //   //     const res = await Api.put(`liked/${userId}`, {
+  //   //       otherUserId: likedId,
+  //   //     });
+  //   //     setCountLike(res.data.likeCount);
+  //   //     setLike(res.data.status);
+  //   //   },
+  //   //   [likedId, userId]
+  //   // );
+  //   const onDelete = () =>{
+  //     console.log("del")
+  //   }
+  //   const onLike = () =>{
+  //     console.log("like")
+  //     let res = {lId:1, uId:1}
+  //     setLikeId(res.lId)
+  //   }
+  //   const handleClick = () => {
+  //     like? onDelete() : onLike()
+  //     setLike(!like)
+  //   };
 
-//   //     const res = await Api.put(`liked/${userId}`, {
-//   //       otherUserId: likedId,
-//   //     });
-//   //     setCountLike(res.data.likeCount);
-//   //     setLike(res.data.status);
-//   //   },
-//   //   [likedId, userId]
-//   // );
-//   const onDelete = () =>{
-//     console.log("del")
-//   }
-//   const onLike = () =>{
-//     console.log("like")
-//     let res = {lId:1, uId:1}
-//     setLikeId(res.lId)
-//   }
-//   const handleClick = () => {
-//     like? onDelete() : onLike()
-//     setLike(!like)
-//   };
-  
   return (
     <div>
-      <div onClick={()=>{
-        
-        Api.post('liked',{
-          challengeId
-        }).then((res)=>setCheckUserId(res))
-        Api.get(`likedCount/:${challengeId}`).then((res)=>console.log('좋아여개수',res))
-        
-        console.log(countLike)
-      }} id="userLike"className="likeButton">
+      <div
+        onClick={() => {
+          Api.post("liked", {
+            challengeId,
+          }).then((res) => setCheckUserId(res));
+          Api.get(`likedCount/${challengeId}`).then((res) =>
+            console.log("좋아여개수", res)
+          );
+
+          console.log(countLike);
+        }}
+        id="userLike"
+        className="likeButton"
+      >
         {likeStatus ? <img src={likedimg} /> : <img src={likeimg} />}
         <span className="ms-2">{countLike} Likes</span>
       </div>
-      
     </div>
   );
 }
