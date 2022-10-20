2232c14a785624bb0a3b4b6e0f78d56819daebd8
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-10-20 17:04:46 +0900

    joinedChallenge 재작업

--- back/src/routers/joinedChallengeRouter.js
+++ back/src/routers/joinedChallengeRouter.js
@@ -3,12 +3,10 @@ import { joinedChallengeService } from "../services/joinedChallengeService";
 import { loginRequired } from "../middlewares/loginRequired";
 import { addImage } from "../middlewares/addImage";
 
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
 const joinedChallengeRouter = Router();
 const upload = addImage("uploads");
 
+// 참가한 챌린지 사진 추가하기 // 1개 추가..
 joinedChallengeRouter.post(
   "/:id",
   loginRequired,
@@ -16,27 +14,25 @@ joinedChallengeRouter.post(
   async (req, res, next) => {
     try {
       const userId = req.currentUserId;
-
       const image = req.file.path;
+
+      const { description } = req.body;
 
       if (image === undefined) {
         return res.status(400).send("이미지가 존재하지 않습니다.");
       }
+
       const { id } = req.params;
-      const countUpload = await prisma.joinedChallenge.count({
-        where: {
-          chalngId: Number(id),
-        },
+      const countUploads = await joinedChallengeService.count({
+        id,
       });
-      const addedImage = `uploads/${image}`;
-      const description = req.body.description;
+
       const addImage = await joinedChallengeService.addChallenge({
         id,
         userId,
-        countUpload,
-        addedImage,
+        countUploads,
+        image,
         description,
-        // challenges,
       });
 
       res.status(200).json({ addImage });
@@ -46,72 +42,34 @@ joinedChallengeRouter.post(
   }
 );
 
-// 참가한 챌린지 사진 추가하기 // 1개 추가..
-// joinedChallengeRouter.post(
-//   "/:id",
-//   loginRequired,
-//   upload.single("image"),
-//   async (req, res, next) => {
-//     try {
-//       const userId = req.currentUserId;
-//       const image = req.file.path;
+joinedChallengeRouter.get(
+  "/info/:challengeId",
+  loginRequired,
+  async (req, res) => {
+    try {
+      const userId = req.currentUserId;
+      const { challengeId } = req.params;
 
-//       if (image === undefined) {
-//         return res.status(400).send("이미지가 존재하지 않습니다.");
-//       }
+      // 인증한 사진 관련된 챌린지 정보
+      const showChallenge = await joinedChallengeService.findChallenge(
+        challengeId
+      );
 
-//       const { id } = req.params;
-//       const countUploads = await prisma.joinedChallenge.count({
-//         where: {
-//           chalngId: Number(id),
-//         },
-//       });
-
-//       const addImage = await prisma.joinedChallenge.create({
-//         data: {
-//           countUpload: countUploads,
-//           addedImage: `uploads/${image}`,
-//           description: req.body.description,
-//           challenges: {
-//             connect: {
-//               challengeId: Number(id),
-//             },
-//           },
-//         },
-//       });
-
-//       res.status(200).json({ addImage });
-//     } catch (error) {
-//       next(error);
-//     }
-//   }
-// );
-
-// challenge 정보
-joinedChallengeRouter.get("/info/:challengeId", async (req, res) => {
-  try {
-    const { challengeId } = req.params;
-
-    // 인증한 사진 관련된 챌린지 정보
-    const showChallenge = await prisma.challenge.findUnique({
-      where: { challengeId: Number(challengeId) },
-    });
-
-    res.status(200).json({ showChallenge });
-  } catch (error) {
-    res.json({ message: error.message });
+      res.status(200).json({ showChallenge });
+    } catch (error) {
+      res.json({ message: error.message });
+    }
   }
-});
+);
 
-// 인증 사진들 전체 보여주기
-joinedChallengeRouter.get("/:challengeId", async (req, res) => {
+joinedChallengeRouter.get("/:challengeId", loginRequired, async (req, res) => {
   try {
+    const userId = req.currentUserId;
     const { challengeId } = req.params;
 
     // 해당 챌린지의 인증 정보 전부가져오는 코드
-    const showCompletedChallenge = await prisma.joinedChallenge.findMany({
-      where: { chalngId: Number(challengeId) },
-    });
+    const showCompletedChallenge =
+      await joinedChallengeService.findJoinedChallenges(challengeId);
 
     res.status(200).json({ showCompletedChallenge });
   } catch (error) {
--- back/src/services/joinedChallengeService.js
+++ back/src/services/joinedChallengeService.js
@@ -1,23 +1,117 @@
-import { joinedChallenge } from "../models/joinedChallenge";
+import { challenge } from "../models/challenge";
 import { dayCountsBetweenTodayAnd } from "../middlewares/dayCountsBetweenTodayAnd";
 
-class joinedChallengeService {
+class challengeService {
+  // create/ post
   static async addChallenge({
-    id,
-    countUpload,
-    addedImage,
+    holdUserId,
+    title,
     description,
-    // challenges,
+    fromDate,
+    toDate,
+    mainImg,
+    explainImg,
   }) {
-    const createdChallenge = await challenge.create({
-      id,
-      countUpload,
-      addedImage,
+    const newChallenge = {
+      holdUserId,
+      title,
       description,
-      //   challenges,
+      fromDate,
+      toDate,
+      startRemainingDate: dayCountsBetweenTodayAnd(fromDate),
+      endRemainingDate: dayCountsBetweenTodayAnd(toDate) * -1,
+      mainImg,
+      explainImg,
+    };
+    const createdChallenge = await challenge.create({
+      newChallenge,
     });
     return createdChallenge;
   }
+
+  // 전체 불러오기 (get)
+  static async getChallenges() {
+    const challenges = await challenge.findMany();
+    return challenges;
+  }
+
+  // Get (진행중인 챌린지 전체)
+  static async getOngoing() {
+    const challenges = await challenge.findOngoing();
+
+    return challenges;
+  }
+  // id 값을 게시물 1개 선택하기(params 값을 이용)
+  static async findUniqueId(id) {
+    const findId = await challenge.findUnique(id);
+    if (!findId) {
+      const error = new Error("invalid id");
+      throw error;
+    }
+    return findId;
+  }
+
+  // Delete
+  // static async deleteOne(id) {
+  //   // const findId = await challenge.findFromDate(id);
+  //   // if (dayCountsBetweenTodayAnd(fromDate) >= 0) {
+  //   //   const error = new Error("cannot modify it after the challenge begins.");
+  //   //   throw error;
+  //   // }
+  //   const deleteChallenge = await challenge.delete(id);
+
+  //   return deleteChallenge;
+  // }
+
+  static async addImage(id, addedImage) {
+    const createdChallenge = await challenge.create({
+      id,
+      addedImage,
+    });
+    return createdChallenge;
+  }
+
+  // static async findUniqueUser(userId, id) {
+  //   const findUserId = await challenge.findUniqueUser(userId, id);
+  //   return findUserId;
+  // }
+
+  static async updateChallenge({
+    id,
+    title,
+    description,
+    fromDate,
+    toDate,
+    titleImg,
+    explainImgs,
+  }) {
+    const updated = await challenge.update({
+      id,
+      title,
+      description,
+      fromDate,
+      toDate,
+      titleImg,
+      explainImgs,
+    });
+    return updated;
+  }
+
+  static async findUniqueChallenge(id) {
+    const findId = await challenge.findUnique(id);
+    return findId;
+  }
+
+  // 전날까지만 수정 가능 (시작 당일 이후로 수정 불가)
+  static async findStartDate(id) {
+    const findUniqueId = await challenge.findUnique(id);
+    const blockUpdate = findUniqueId.startRemainingDate;
+    if (blockUpdate >= 0) {
+      const errorMessage = "cannot modify it after the challenge begins.";
+      return errorMessage;
+    }
+    return blockUpdate;
+  }
 }
 
-export { joinedChallengeService };
+export { challengeService };
