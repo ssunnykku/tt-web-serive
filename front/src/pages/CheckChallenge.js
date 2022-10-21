eda72ff4010fdb9c75728e6bd57417535e84f1b9
geunhyuk park <rmsgur95@gmail.com>
2022-10-21 21:53:59 +0900

    오류확인

--- front/src/pages/CheckChallenge.js
+++ front/src/pages/CheckChallenge.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import NetworkCard from "../components/NetworkCard";
-
+import { useParams } from "react-router-dom";
 import "../styles/checkChallenge.css";
 import * as Api from '../api'
 
 const CheckChallenge = () => {
+    let {id}=useParams();
     const [mainImg,setMainImg]=useState('')
     const [remainDate,setRemainDate]=useState()
-    const [title,setTitle]=useState('')
+    const [title,setTitle]=useState('제목')
     const [playPoint,setPlayPoint]=useState(0);
     const [expectedPoint,setExpectedPoint]=useState(0);
-
+    const [data,setData]=useState([]);
+    // useEffect(()=>{
+      // Api.get(`challenges/mine/:${id}`).then((res)=>setData(res))
+    // },[])
+    // Api.get(`challenges/mine/:${id}`).then((res)=>console.log(res))
+    
   return (
     <div className="checkChallenge">
       <NavBar />
@@ -21,8 +27,10 @@ const CheckChallenge = () => {
       </div>
       <div className="checkContents">
         <div className="mainimg">
-            <div className="imgsize"></div>
-            <h1> 안녕</h1>
+            <div className="imgsize">
+              {/* <NetworkCard /> */}
+            </div>
+            
         </div>
         <div className="mainContent">
           <div className="dropdowncontents"></div>
