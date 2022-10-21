d7672ad1eef1ab066fe073acd2f780d668d3b5c0
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-10-21 23:15:21 +0900

    백엔드 머지

--- back/src/routers/joinedChallengeRouter.js
+++ back/src/routers/joinedChallengeRouter.js
@@ -30,8 +30,6 @@ joinedChallengeRouter.post(
           });
         });
 
-      const { description } = req.body;
-
       if (image === undefined) {
         return res.status(400).send("이미지가 존재하지 않습니다.");
       }
