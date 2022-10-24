94424c42ecca0a54dfe5c268f8a1f73c40a42485
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-10-24 22:13:27 +0900

    back/addImage.js 수정

--- back/src/middlewares/addImage.js
+++ back/src/middlewares/addImage.js
@@ -8,19 +8,20 @@ function addImage(imgPath) {
       console.error("fail to make uploads folder.");
     }
   });
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + "-" + Date.now() + ext);
-    },
-    limits: { fileSize: 10 * 1024 * 1024 },
-  });
+  const storage = multer({ dest: "uploads/" });
+  // const storage = multer.diskStorage({
+  //   destination: function (req, file, cb) {
+  //     cb(null, "uploads/");
+  //   },
+  //   filename: function (req, file, cb) {
+  //     const ext = path.extname(file.originalname);
+  //     cb(null, path.basename(file.originalname, ext) + "-" + Date.now() + ext);
+  //   },
+  //   limits: { fileSize: 10 * 1024 * 1024 },
+  // });
 
-  return multer({ storage: storage });
-  // return storage;
+  // return multer({ storage: storage });
+  return storage;
 }
 
 export { addImage };
