4b7299115f7f384f4be8577bce1deca4c19e6905
HaEun <rjadmszhd777@naver.com>
2022-11-09 16:34:34 +0900

    11/9 수정하기 버튼

--- front/src/components/Card/MyChallengeCard.js
+++ front/src/components/Card/MyChallengeCard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
 import "../../styles/network/networkcard.css";
-import * as Api from '../../api'
+import * as Api from "../../api";
 import UserLike from "../UserLike";
 const MychallengeCard = ({ item }) => {
   var today = new Date();
@@ -10,16 +10,18 @@ const MychallengeCard = ({ item }) => {
   var month = ("0" + (today.getMonth() + 1)).slice(-2);
   var day = ("0" + today.getDate()).slice(-2);
   var dateString = year + "-" + month + "-" + day;
-  let navigate=useNavigate()
+  let navigate = useNavigate();
   const mainImg = item.challenge.mainImg;
   const [person, setPerson] = useState();
-  const [user,setUser]=useState('')
+  const [user, setUser] = useState("");
   useEffect(() => {
-    Api.get(`countJoinUser/${item.challenge.challengeId}`).then((res) => setPerson(res));
+    Api.get(`countJoinUser/${item.challenge.challengeId}`).then((res) =>
+      setPerson(res)
+    );
     Api.get("currentUser").then((res) => setUser(res.data));
   }, []);
-  console.log(item.challenge.holdUserId)
-  console.log(user.userId)
+  console.log(item.challenge.holdUserId);
+  console.log(user.userId);
   return (
     <Card
       id="cardBody12"
@@ -39,14 +41,10 @@ const MychallengeCard = ({ item }) => {
     >
       <Card.Body>
         <div className="imageWrap">
-          <Card.Img
-            className="mb-3 imgSize"
-            src={mainImg}
-            alt="대표 사진"
-          />
+          <Card.Img className="mb-3 imgSize" src={mainImg} alt="대표 사진" />
 
           <Card.Title>{item?.challenge.title}</Card.Title>
-        
+
           <div className="cardtext">
             👨‍👧‍👧 {person?.data}
             <UserLike challengeId={item.challenge.challengeId} />
@@ -57,22 +55,23 @@ const MychallengeCard = ({ item }) => {
             </a>{" "}
           </div>
           <button
-           
             className="networkButton"
-            onClick={()=>{
-                navigate(`/checkChallenge/${item.challenge.challengeId}`)
+            onClick={() => {
+              navigate(`/checkChallenge/${item.challenge.challengeId}`);
             }}
-            
           >
             인증하기
           </button>
-          {user.userId===item.challenge.holdUserId?
-          <button className="editButton">수정하기</button>
-          :
-          null
-          }
-          
-          
+          {user.userId === item.challenge.holdUserId ? (
+            <button
+              className="editButton"
+              onClick={() => {
+                navigate(`/editChallenge/${item.challenge.challengeId}`);
+              }}
+            >
+              수정하기
+            </button>
+          ) : null}
         </div>
       </Card.Body>
     </Card>
